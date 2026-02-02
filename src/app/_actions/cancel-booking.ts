02750842b4c635c3ff8a25f7552b9910e316de63
Diego Ewerton <diegogeniu@gmail.com>
2026-02-02 15:47:11 -0300

    atualização

--- src/app/_actions/cancel-booking.ts
+++ src/app/_actions/cancel-booking.ts
@@ -3,7 +3,6 @@
 
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/_lib/auth";
-import { BookingStatus } from "@prisma/client";
 import { db } from "@/app/_lib/prisma";
 
 export async function cancelBooking(bookingId: string) {
@@ -20,15 +19,16 @@ export async function cancelBooking(bookingId: string) {
   if (!booking) throw new Error("Booking not found");
   if (booking.userId !== userId) throw new Error("Forbidden");
 
-  if (booking.status === BookingStatus.FINALIZADO) {
+  if (booking.status === "FINALIZADO") {
     throw new Error("Não é possível cancelar uma reserva finalizada.");
   }
 
   await db.booking.update({
     where: { id: bookingId },
-    data: { status: BookingStatus.PENDENTE }, // ou você pode criar CANCELADO se quiser
+    data: { status: "PENDENTE" }, // ou você pode criar CANCELADO se quiser
   });
 
   // Se você quer realmente "liberar horário" removendo do banco:
   // await db.booking.delete({ where: { id: bookingId } });
 }
+
